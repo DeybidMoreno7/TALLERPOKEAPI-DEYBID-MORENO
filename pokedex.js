4aaeb2352db533455f2f7d8279412d038bfc6201
Deybid Santiago Moreno Ruiz <ingdeybid.moreno@gmail.com>
2026-07-06 22:42:26 -0500

    practicando ramas - parte 3 laboratorio

--- pokedex.js
+++ pokedex.js
@@ -6,10 +6,10 @@ async function iniciar() {
 
     while (bandera) {
 
-        const nombrePokemon = prompt("Digite el nombre del pokemon a consultar: ");
+        let nombrePokemon = prompt("Digite el nombre del pokemon a consultar: ");
 
-        await buscarPokemon(nombrePokemon);
-        await mostrarFicha(datos);
+        const pokemonDatos = await buscarPokemon(nombrePokemon);
+        await mostrarFicha(pokemonDatos);
         const confirmacion = prompt("Desea hacer una nueva consulta? S/N: ");
 
         if (confirmacion.toUpperCase() === "N") {
@@ -23,7 +23,7 @@ async function buscarPokemon(nombrePokemon) {
         "https://pokeapi.co/api/v2/pokemon/" + nombrePokemon
     );
 
-    console.log("Status:", queryPokemon.status);
+    console.log("Query Status:", queryPokemon.status);
 
     if (!queryPokemon.ok) {
         console.log("No encontrado");
@@ -32,16 +32,61 @@ async function buscarPokemon(nombrePokemon) {
 
     const datos = await queryPokemon.json();
     return datos;
+    // console.log("Nombre:", datos.name);
+    // console.log("ID:", datos.id);
 }
 
-async function mostrarFicha(datos){
-    if(datos === null){
-        console.log("No hay datos por mostrar");
+
+async function mostrarFicha(pokemonDatos){
+    if(pokemonDatos === null){
+        console.log("Nada que mostrar");
         return;
     }
-    console.log(datos.name.toUpperCase);
-    console.log(datos.id);
+    console.log("Nombre Pokemon: "+pokemonDatos.name.toUpperCase());
+    console.log("ID Pokemon:", pokemonDatos.id);
+    let tiposPokemon = [];
+    /*
+    for(const i of pokemonDatos.types){
+        tiposPokemon = [tiposPokemon , [i.type.name]]; // I tried like this, but is not the better way :u
+    }
+    
+    */ 
+    for(const i of pokemonDatos.types){
+        tiposPokemon.push(i.type.name);//agregamos al final del array cada elemento nuevo :D
+    }
+    console.log("\x1b[1m%s\x1b[0m","... Separando elementos del array con / ...")
+    console.log("Imprimiendo tipos de pokemon desde una cadena"+tiposPokemon.join(" / "));
+    let alturaPokemon = (pokemonDatos.height * 10);
+    let pesoPokemon = (pokemonDatos.weight / 10);
+    console.log("Altura Pokemon: "+alturaPokemon+" cms");
+    console.log("Peso Pokemon: "+pesoPokemon+" kgs");
+    let numero = 0;
+    for(const i of pokemonDatos.stats){
+        numero++;
+        console.log("Estadística #"+numero+" Nombre estadística: "+i.stat.name+ 
+            " - Valor estadística: "+i.base_stat);
+    }
+    numero = 0;
+    for(const i of pokemonDatos.abilities){
+        numero++;
+        if(i.is_hidden===true){
+            console.log("Habilidad #"+numero+": "+i.ability.name+" (oculta)");
+        }else{
+            console.log("Habilidad #"+numero+": "+i.ability.name);
+        }
+        
+    }
 }
+
+// async function mostrarFicha(datos){
+//     if(datos === null){
+//         console.log("No hay datos por mostrar");
+//         return;
+//     }
+//     console.log(datos.name.toUpperCase);
+//     console.log(datos.id);
+// }
+
 
 
 
